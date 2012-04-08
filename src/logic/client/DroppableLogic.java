48a934d9f0e76437bc81489610198bd956e4f8a5
Michael <Michael@localhost>
2012-04-08 22:28:13 +0300

    (no commit message)

--- src/logic/client/DroppableLogic.java
+++ src/logic/client/DroppableLogic.java
@@ -1,17 +1,17 @@
-package logic.client;
-
-import java.util.ArrayList;
-
-import logic.card.Card;
-
-public class PublicArea {
-	private ArrayList<Card> cards;
-	
-	
-	public ArrayList<Card> getCards() {
-		return cards;
-	}
-	public void add(String userName,Card card){
-		cards.add(card);
-	}
-}
+//package logic.client;
+//
+//import java.util.ArrayList;
+//
+//import logic.card.Card;
+//
+//public class PublicArea {
+//	private ArrayList<Card> cards;
+//	
+//	
+//	public ArrayList<Card> getCards() {
+//		return cards;
+//	}
+//	public void add(String userName,Card card){
+//		cards.add(card);
+//	}
+//}
