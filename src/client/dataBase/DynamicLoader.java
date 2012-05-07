dfc199f108133a88390c0a289f07809d6b6fb7df
Yoav <Yoav@localhost>
2012-05-08 00:17:34 +0300

    dynamic loading added

--- src/client/dataBase/DynamicLoader.java
+++ src/client/dataBase/DynamicLoader.java
@@ -1,6 +1,7 @@
 package client.dataBase;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -8,8 +9,16 @@ import java.net.URLClassLoader;
 import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
 import communication.actions.AddPlayerAction;
 
@@ -20,35 +29,50 @@ import war.War;
 
 
 public class DynamicLoader {
+	final static String PLUGINDIR="plugins/";
+	private HashMap<String, String> mapping;
 	
-	
-
-	public static void addJar(String path) throws Exception{
-		try {
-			String urlPath="jar:file://"+"C:\\googleworkspace\\warGame\\war.jar"+"!/";
-			
-			URL u = new URL(urlPath);
-			URLClassLoader sysLoader = (URLClassLoader) ClassLoader
-			.getSystemClassLoader();
-			URL urls[] = sysLoader.getURLs();
-			for (int i = 0; i < urls.length; i++) {
-			if ((urls[i].toString().equalsIgnoreCase(u.toString()))) {
-			//if (log.isDebugEnabled()) {
-			//log.debug("URL " + u + " is already in the CLASSPATH");
-			//}
-				return;
-			}}
-			
-			Class sysclass = URLClassLoader.class;
-			Method method = sysclass.getDeclaredMethod("addURL", new Class[]{URL.class});
-			method.setAccessible(true);
-			method.invoke(sysLoader, new Object[] { u });
-			}
-			catch (Exception e) {
-			e.printStackTrace();
-			throw new Exception( "Error, could not add URL to system classloader"+ e.getMessage());
-			}
+	public DynamicLoader() {
+		mapping=new HashMap<String, String>();
 	}
+	
+	private void mapGame(String gameName, URL[] urls){
+		try{				
+				ClassLoader cl = new URLClassLoader(urls);
+				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder builder = factory.newDocumentBuilder();
+				Document doc = builder.parse(cl.getResource("info.xml").openStream());
+				NodeList classPathNode=doc.getElementsByTagName("classPath");
+				if (classPathNode.getLength()==1){
+					mapping.put(gameName, 
+							classPathNode.item(0).getFirstChild().getNodeValue().trim());
+				}			
+		 }
+		 catch (Exception e){	
+			 e.printStackTrace();
+		 }
+	}
+	
+	public Set<String> getGameNames(){
+		mapPlugins();
+		return mapping.keySet();
+	}
+	
+	
+	private void mapPlugins(){
+		try {
+			File folder = new File(PLUGINDIR);		
+			for (File file : folder.listFiles()){		
+				URL url=file.toURI().toURL();						
+				URL[] urls = new URL[]{url};				
+				mapGame(file.getName().substring(0, file.getName().lastIndexOf(".")),urls);
+			}		
+			
+		} catch (IOException e) {			
+			e.printStackTrace();
+		}				
+	}
+	
 	/**
 	 * loading an entire jar file with all its classes
 	 * @param path path of jar file to load
@@ -56,74 +80,37 @@ public class DynamicLoader {
 	 * @see Game
 	 * @return game instance
 	 */
-	public static Game LoadPlugin(String path,String gameClassName){
+	public Game LoadPlugin(String gameName){		
+		
 		Game game=null;
 		try {
-		
-		File file  = new File("C:\\googleworkspace\\warGame\\war.jar");
-		 URL url;		
-			url = file.toURI().toURL();
-				
-		 URL[] urls = new URL[]{url};
-		 ClassLoader cl = new URLClassLoader(urls);
-		 Class cls = cl.loadClass("war.War");		
-			 game=(Game)cls.newInstance();
+			File file = new File(PLUGINDIR+"/"+gameName+".jar");
+			URL url = file.toURI().toURL();				
+			URL[] urls = new URL[]{url};
+			//get the classPath that extends Game class
+			String classPath=mapping.get(gameName);
+			
+			//check if never mapped that game before
+			if (classPath==null){
+				/*we are not the host, therefore we didn't
+				 *map all plug-ins.*/
+				mapGame(gameName,urls);
+			}
+			
+			ClassLoader cl = new URLClassLoader(urls);	
+			Class<?> cls = cl.loadClass(mapping.get(gameName));		
+			game=(Game)cls.newInstance();
 		 
 		 
 		} catch (MalformedURLException e) {			
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-		} catch (InstantiationException e) {			
+		} catch (InstantiationException e) {
 			e.printStackTrace();
-		} catch (IllegalAccessException e) {
+		} catch (IllegalAccessException e) {			
 			e.printStackTrace();
-		}
+		}		
 		return game;
-	}
-	
-	public static ArrayList<String>getClassNamesFromPackage(String packageName) throws IOException{
-	    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-	    URL packageURL;
-	    ArrayList<String> names = new ArrayList<String>();;
-
-	    packageName = packageName.replace(".", "/");
-	    packageURL = classLoader.getResource("war.jar");
-
-	   // if(packageURL.getProtocol().equals("jar")){
-	        String jarFileName;
-	        JarFile jf ;
-	        Enumeration<JarEntry> jarEntries;
-	        String entryName;
-
-	        // build jar file name, then loop through zipped entries
-	        jarFileName = URLDecoder.decode(packageURL.getFile(), "UTF-8");
-	        //jarFileName = jarFileName.substring(5,jarFileName.indexOf("!"));
-	        System.out.println(">"+jarFileName);
-	        jf = new JarFile(jarFileName);
-	        jarEntries = jf.entries();
-	        while(jarEntries.hasMoreElements()){
-	            entryName = jarEntries.nextElement().getName();
-	            if(entryName.startsWith("war") && entryName.length()>packageName.length()+5){
-	                entryName = entryName.substring(4,entryName.lastIndexOf('.'));
-	                names.add(entryName);
-	            }
-	        }
-
-	    // loop through files in classpath
-	   // }else{
-//	        File folder = new File(packageURL.getFile());
-//	        File[] contenuti = folder.listFiles();
-//	        String entryName1;
-//	        for(File actual: contenuti){
-//	            entryName1 = actual.getName();
-//	            entryName1 = entryName1.substring(0, entryName1.lastIndexOf('.'));
-//	            names.add(entryName1);
-//	        }
-	        for (String name : names){
-	        	LoadPlugin("", name);
-	        }
-	   // }
-	    return names;
 	}
 }
