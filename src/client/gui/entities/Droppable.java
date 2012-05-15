6e3af31c8770e17c8c0c42f843c61bd7e673770e
Yoav <Yoav@localhost>
2012-05-15 19:33:30 +0300

    (no commit message)

--- src/client/gui/entities/Droppable.java
+++ src/client/gui/entities/Droppable.java
@@ -37,10 +37,9 @@ public abstract class Droppable{
 	public void onDrop(Player player,Droppable from, Card card){
 		from.removeCard(player,card);
 		card.setCoord(getX(), getY());
-		addCard(player,card);
-		ArrayList<Card>cards=new ArrayList<Card>();
-		cards.add(card);				
 		ClientController.sendAPI().cardAdded(card, from.getMyId(),getMyId(),player);
+		addCard(player,card);						
+		
 	}
 	
 	public void onCardAdded(Player byWhom, Card card) {
