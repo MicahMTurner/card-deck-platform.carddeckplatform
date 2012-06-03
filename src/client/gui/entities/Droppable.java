af3487d833f456b8bc7a4b0a962664aac8893870
davidhodeffi <davidhodeffi@localhost>
2012-06-04 00:19:15 +0300

    made normalisation of position on the layout

---
+++ src/client/gui/entities/Droppable.java
@@ -0,0 +1,176 @@
+package client.gui.entities;
+
+import java.io.Serializable;
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Stack;
+
+import org.newdawn.slick.geom.Line;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
+
+import utils.Card;
+import utils.Player;
+import utils.Point;
+import utils.Position;
+import utils.droppableLayouts.DroppableLayout;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import carddeckplatform.game.BitmapHolder;
+import client.controller.ClientController;
+
+public abstract class Droppable implements Serializable {
+	/**
+	 * **************************************************<br/>
+	 * **************************************************<br/>
+	 * ****18**********************************19********<br/>
+	 * ****************1**2**3**4**5*********************<br/>
+	 * ****************6**7**8**9**10********************<br/>
+	 * ****************11*12*13*14*15********************<br/>
+	 * **************************************************<br/>
+	 * ****16**********************************17********<br/>
+	 * **************************************************<br/>
+	 * 
+	 * @author Yoav
+	 * 
+	 */
+
+	// protected Point point;
+	// public Stack<GuiCard>guiCards=new Stack<GuiCard>();
+	// protected Stack<Card> cards = new Stack<Card>();
+	// protected ArrayList<Card> cards;
+	protected transient Shape shape = null;
+	protected int id;
+	protected Position position;
+	protected Point scale;
+	protected String image;
+	protected transient DroppableLayout droppableLayout;
+
+	public Shape getShape() {
+		if (shape == null) {
+			Point size = MetricsConvertion.pointRelativeToPx(this.scale);
+			shape = new Rectangle(getX() - (size.getX() / 2), getY()
+					- (size.getY() / 2), size.getX(), size.getY());
+		}
+		return shape;
+	}
+
+	public void onDrop(Player player, Droppable from, Card card) {
+		from.removeCard(player, card);
+		// card.setCoord(getX(), getY());
+		ClientController.sendAPI().cardAdded(card, from.getId(), id, player);
+		addCard(player, card);
+		rearrange(0);
+
+	}
+
+	public boolean isContain(float x, float y) {
+		// return shape.contains(x, y);
+		return getShape().contains(x, y);
+
+	}
+
+	public boolean isIntersect(Line line) {
+		// try {
+		// return line.intersects(shape)||shape.contains(line);
+		return line.intersects(getShape()) || getShape().contains(line);
+		// } catch (Exception e) {
+		// // TODO: handle exception
+		// // TODO: handle exception
+		// this.shape=getShape();
+		// return line.intersects(shape)||shape.contains(line);
+		// }
+
+	}
+
+	public Position getPosition() {
+		return position;
+	}
+
+	public int indexOfDraggabale(Draggable draggable) {
+
+		return getCards().indexOf(draggable);
+
+	}
+
+	public void onCardAdded(Player byWhom, Card card) {
+		// card.setCoord(getX(),getY());
+		addCard(byWhom, card);
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (other == null || !(other instanceof Droppable)) {
+			return false;
+		}
+		Droppable otherDroppable = (Droppable) other;
+		return this.id == otherDroppable.id;
+	}
+
+	public abstract void deltCard(Card card);
+
+	public abstract AbstractList<Card> getCards();
+
+	public abstract void addCard(Player player, Card card);
+
+	public abstract void removeCard(Player player, Card card);
+
+	public void draw(Canvas canvas, Context context) {
+		Bitmap img = BitmapHolder.get().getBitmap(image);
+		Matrix matrix = new Matrix();
+
+		Point absScale = MetricsConvertion.pointRelativeToPx(scale);
+
+		matrix.postScale((float) absScale.getX() / (float) img.getWidth(),
+				(float) absScale.getY() / (float) img.getHeight());
+		matrix.postTranslate(getX() - absScale.getX() / 2,
+				getY() - absScale.getY() / 2);
+
+		canvas.drawBitmap(img, matrix, null);
+		// canvas.drawBitmap(img,getX()-(img.getWidth() /
+		// 2),getY()-(img.getHeight() / 2),null);
+	}
+
+	public Droppable(int id, Position position, Point scale) {
+		this.id = id;
+		this.scale = scale;
+		this.position = position;
+		// this.shape=getShape();
+		// this.cards=new ArrayList<Card>();
+		// this.point=new Point(190,175);
+		// this.myId=IDMaker.getMaker().getId(position);
+	}
+
+	public float getX() {
+		return MetricsConvertion.pointRelativeToPx(position.getPoint()).getX();
+	}
+
+	public float getY() {
+		return MetricsConvertion.pointRelativeToPx(position.getPoint()).getY();
+	}
+
+	public void rearrange(int index) {
+		if (droppableLayout != null)
+			droppableLayout.rearrange(index, getShape().getWidth(), getShape()
+					.getHeight());
+
+	}
+
+	public abstract int cardsHolding();
+
+	public abstract boolean isEmpty();
+
+	public abstract void clear();
+
+	public int getId() {
+		return id;
+	}
+
+	public void setPosition(Position relativePosition) {
+		this.position = relativePosition;
+
+	}
+
+}
---
+++ src/utils/Player.java
@@ -0,0 +1,165 @@
+package utils;
+
+import handlers.PlayerEventsHandler;
+
+import java.util.ArrayList;
+import communication.link.ServerConnection;
+import communication.messages.SwapRequestMessage;
+
+
+
+import org.newdawn.slick.geom.Circle;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
+
+import utils.droppableLayouts.BottomLineLayout;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import carddeckplatform.game.BitmapHolder;
+import carddeckplatform.game.R;
+import carddeckplatform.game.gameEnvironment.PlayerInfo;
+import client.controller.ClientController;
+import client.controller.LivePosition;
+import client.gui.entities.Droppable;
+import client.gui.entities.MetricsConvertion;
+
+
+
+
+public class Player extends Droppable implements  Comparable<Player>{
+	private PlayerEventsHandler handler;	
+	private ArrayList<Card> hand;
+	boolean myTurn;
+	private PlayerInfo playerInfo;
+	private Position.Player globalPosition;
+	
+	
+	
+	
+	
+	public Player(PlayerInfo playerInfo,Position.Player globalPosition,int uniqueId, PlayerEventsHandler handler) {
+		super(uniqueId,Position.Player.BOTTOM, new Point(40,13));
+		this.playerInfo=playerInfo;		
+		this.globalPosition=globalPosition;
+		this.position=Position.Player.BOTTOM;	
+		this.handler=handler;
+		this.hand=new ArrayList<Card>();
+		this.myTurn=false;	
+		
+		this.image = "playerarea";
+		
+		this.droppableLayout = new BottomLineLayout(this);
+		//BitmapHolder.get().scaleBitmap(image, this.scale);
+	}
+	
+	public Position.Player getGlobalPosition() {
+		return globalPosition;
+	}
+	public PlayerEventsHandler getHandler() {
+		return handler;
+	}
+	public Position.Player getPosition() {
+		return (Position.Player)position;
+	}
+	public String getUserName() {
+		return playerInfo.getUsername();
+	}
+	public int getId() {
+		return id;
+	}
+	public void addCard(Card card) {
+		card.setOwner((Position.Player)position);		
+		hand.add(card);		
+		handler.onCardAdded(this, card);		
+	}
+	
+//	public void setCoord(Point coord) {
+//		this.coord = coord;
+//	}
+	private void removeCard(Card card) {
+		hand.remove(card);
+		handler.onCardRemoved(this, card);
+		
+	}
+	
+	public void setGlobalPosition(Position.Player globalPosition) {
+		this.globalPosition = globalPosition;
+		
+	}
+	
+	public boolean isMyTurn() {
+		return myTurn;
+	}
+	public void startTurn(){
+		myTurn=true;		
+		ClientController.get().enableUi();
+	}
+	public void endTurn(){
+		myTurn=false;		
+		ClientController.sendAPI().endTurn(globalPosition);
+	}
+	public void deltCard(Card card) {
+		card.setOwner((Position.Player)position);
+		hand.add(card);	
+		card.setLocation(getX(), getY());
+	}
+	public int cardsHolding() {		
+		return hand.size();
+	}
+
+	
+	public boolean isEmpty() {
+		return hand.isEmpty();
+	}
+
+	
+	public void clear() {
+		hand.clear();
+		
+	}
+	@Override
+	public int compareTo(Player another) {
+		return this.globalPosition.compareTo(another.getGlobalPosition());
+	}
+	
+	public void setRelativePosition(utils.Position.Player devicePlayerGlobalPos) {
+		Position.Player newPos=this.globalPosition.getRelativePosition(devicePlayerGlobalPos);;
+		
+		if (!(this.globalPosition.equals(devicePlayerGlobalPos))){			
+			for (Card card : hand){
+				CardTransformation.get().transform(card,position,newPos);
+			}
+			position=newPos;
+		}		
+	}
+	
+	@Override
+	public void addCard(Player player, Card card) {
+		addCard(card);
+	}
+	@Override
+	public void removeCard(Player player, Card card) {
+		removeCard(card);
+	}
+//	@Override
+//	public void draw(Canvas canvas, Context context) {
+//		Bitmap img = BitmapHolder.get().getBitmap(image);
+//		canvas.drawBitmap(img,getX()-img.getWidth() / 2,getY()-img.getHeight() / 2,null);
+//		
+//	}
+
+	@Override
+	public ArrayList<Card> getCards() {		
+		return hand;
+	}
+
+//	public void setAzimute(double newAzimute) {
+//		playerInfo.setAzimute(globalPosition,newAzimute);
+//	}
+	public Double getAzimute() {
+		return playerInfo.getAzimute();
+	}
+
+}
