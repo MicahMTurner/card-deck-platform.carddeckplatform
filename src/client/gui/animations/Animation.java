7daeac190601130a1c0776c83c44e81a53cdfbea
Yoav <Yoav@localhost>
2012-06-06 04:21:57 +0300

    added thread pool for executing Animation.

--- src/client/gui/animations/Animation.java
+++ src/client/gui/animations/Animation.java
@@ -1,8 +1,11 @@
 package client.gui.animations;
 
+import carddeckplatform.game.gameEnvironment.GameEnvironment;
+
 public abstract class Animation implements Runnable{
 	public void execute(){
-		new Thread(this).start();
+		//new Thread(this).start();
+		GameEnvironment.get().getExecutor().execute(this);
 	}
 	@Override
 	public void run(){
