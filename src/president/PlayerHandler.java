aac1d1603ed89fa7b03120cc3c758df4f851432f
Yoav <Yoav@localhost>
2012-06-13 18:34:55 +0300

    button

---
+++ src/president/PlayerHandler.java
@@ -0,0 +1,66 @@
+package president;
+
+import client.controller.ClientController;
+import utils.Card;
+import utils.Player;
+import utils.Position;
+import utils.Public;
+import handlers.PlayerEventsHandler;
+
+public class PlayerHandler implements PlayerEventsHandler{
+	
+	
+	@Override
+	public boolean onMyTurn(Player player) {
+		if (President.passed){
+			player.endTurn();
+		}else{
+			Card topCard=(ClientController.get().getZone(Position.Public.MID)).peek();
+			//check if no one placed any cards during the entire round
+			if (topCard!=null && topCard.getOwner().getId()==ClientController.get().getMe().getId()){
+				Integer nextPlayerId=ClientController.get().endRound();
+				ClientController.sendAPI().endRound(nextPlayerId);
+				
+			}
+			
+		}
+		return false;
+	}
+
+	@Override
+	public boolean onTurnEnd(Player player) {
+		return false;
+	}
+
+	@Override
+	public boolean onCardAdded(Player player, Card card) {
+		boolean answer=true;
+		if (player.equals(ClientController.get().getMe())){
+			card.reveal();
+			answer=true;
+		}
+		
+		return answer;
+	}
+
+	@Override
+	public boolean onCardRemoved(Player player, Card card) {
+		if (player.equals(ClientController.get().getMe())){
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean onCardRevealed(Player player, Card card) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean onRoundEnd(Player player) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/war/PublicHandler.java
@@ -0,0 +1,61 @@
+package war;
+
+import handlers.PublicEventsHandler;
+
+import java.util.ArrayList;
+
+import utils.Card;
+import utils.Player;
+import utils.Position;
+import utils.Public;
+import utils.StandartCard;
+import client.controller.ClientController;
+
+public class PublicHandler implements PublicEventsHandler{
+	private int cardsPlacedWhileTie=0;
+	private boolean guiLocked=false;
+	
+	@Override
+	public boolean onCardAdded(Public publicArea,Player byWhom, Card card) {
+		boolean answer=false;
+		Card cardInPublic=publicArea.peek();
+
+		if (card.getOwner().equals(byWhom.getPosition())){
+			//no cards in public place or same player adding a card while tie
+			if (cardInPublic==null || (card.getOwner().equals(cardInPublic.getOwner()) && War.tie)){				
+				if (War.tie){
+					if (cardsPlacedWhileTie<2){				
+						card.hide();
+						++cardsPlacedWhileTie;
+					}else{
+						if (ClientController.get().getMe().equals(byWhom)){
+							ClientController.get().disableUi();							
+						}
+					}
+				}else{					
+					cardsPlacedWhileTie=0;
+					card.reveal();
+					ClientController.get().getMe().endTurn();					
+				}
+				answer=true;
+			}
+		}
+		return answer;
+	}
+	
+	@Override
+	public boolean onCardRemoved(Public publicZone, Player player, Card card) {
+		return false;
+	}
+	@Override
+	public boolean onCardRevealed(Public publicZone, Player player, Card card) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean onRoundEnd(Public publicZone, Player player) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
