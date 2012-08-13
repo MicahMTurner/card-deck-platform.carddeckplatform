ef405b83b1ab3374090c36d58fa91e3b841a8c49
davidhodeffi <davidhodeffi@localhost>
2012-08-13 16:45:09 +0300

    ranking + market activities

---
+++ src/carddeckplatform/game/PluginDetails.java
@@ -0,0 +1,41 @@
+package carddeckplatform.game;
+
+public class PluginDetails {
+	public String name;
+	public String date;
+	public String address;
+	int rank;
+	long sum;
+	long numOfvotes;
+	
+	public PluginDetails(String name, String date, String address, long sum,
+			long numOfvotes) {
+		super();
+		this.name = name;
+		this.date = date;
+		this.address = address;
+		this.sum = sum;
+		this.numOfvotes = numOfvotes;
+		System.out.println((double)sum/(numOfvotes*10));
+		this.rank=(int) Math.round((double)sum/(numOfvotes*10)*10)-1;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
