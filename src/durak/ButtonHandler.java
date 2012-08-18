c58d29a4da0471caab76d2ad9804fe3533f19b9e
Irzh <Irzh@localhost>
2012-08-18 21:45:16 +0300

    (no commit message)

--- src/durak/ButtonHandler.java
+++ src/durak/ButtonHandler.java
@@ -15,7 +15,7 @@ public class ButtonHandler implements ButtonEventsHandler {
 		// if I am the attacked player then I may end the round
 		// TODO: add enable timer - enable the end round button only after several seconds after the last card was put in public.
 		if(Durak.isAttacked(ClientController.get().getMe()))	
-			ClientController.get().sendAPI().endRound(5);
+			ClientController.get().sendAPI().endRound();
 		
 	}
 
