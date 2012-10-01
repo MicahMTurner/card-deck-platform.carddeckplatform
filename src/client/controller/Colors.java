9e43cb41350812f4bbfda33ed67a2a9bcbc73ee6
Irzh <Irzh@localhost>
2012-10-01 18:54:44 +0200

    (no commit message)

---
+++ src/client/controller/Colors.java
@@ -0,0 +1,16 @@
+package client.controller;
+
+public class Colors {
+	static final int RED = -65536;
+	static final int BLUE = -16776961;
+	static final int BLACK = -16777216;
+	static final int CYAN = -16711681;
+	static final int DKGRAY = -12303292;
+	static final int GRAY = -7829368;
+	static final int GREEN = -16711936;
+	static final int LTGRAY = -3355444;
+	static final int MAGENTA = -65281;
+	static final int TRANSPARENT = 0;
+	static final int WHITE = -1;
+	static final int YELLOW = -256;
+}
